848380da431a8d90dc71655375e3574b5ca9e2e1
jsulz <j.sulzdorf@gmail.com>
2023-07-30 12:31:47 -0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(500).json({ name: 'John Doe' })
+  await new Promise(r => setTimeout(r, 11000));
+  res.status(200).json({ name: 'John Doe' })
 }
