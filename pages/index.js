9d691b4fd0d0105c0e2c6c566f551837d92ebe82
jsulz <j.sulzdorf@gmail.com>
2023-07-30 12:22:49 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  console.log("error")
+  console.error("error")
   return (
     <div className={styles.container}>
       <Head>
